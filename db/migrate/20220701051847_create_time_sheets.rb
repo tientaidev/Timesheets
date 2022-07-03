329da94134793f9177c499ae8497e8104e2a5396
tientaidev <tientaidev@gmail.com>
2022-07-03 15:20:48 +0700

    Change time to date time in time sheet column migration

--- db/migrate/20220701051847_create_time_sheets.rb
+++ db/migrate/20220701051847_create_time_sheets.rb
@@ -2,8 +2,8 @@ class CreateTimeSheets < ActiveRecord::Migration[7.0]
   def change
     create_table :time_sheets do |t|
       t.date :date
-      t.time :start_time
-      t.time :finish_time
+      t.datetime :start_time
+      t.datetime :finish_time
       t.timestamps
     end
   end
